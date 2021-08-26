35990bd6c951ecb8ed63406ceca4aaf25933457c
YuMichay <julia.one.such@gmail.com>
2021-08-26 21:47:45 +0300

    feat: created code for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = ['(', '{', '['];
+  const bracketCouples = {
+    [')']: '(',
+    ['}']: '{',
+    [']']: '['
+  };
+  let result = [];
+  for (let i = 0; i < str.length; i++){
+    let strSymbol = str[i];
+    if (openBrackets.includes(strSymbol)){
+      result.push(strSymbol);
+    } else{
+      if (result.length===0){
+        return false;
+      }
+      let lastElement = result[result.length-1];
+      if (bracketCouples[strSymbol]===lastElement){
+        result.pop();
+      } else{
+        return false;
+      }
+    }
+  }
+  return result.length===0; 
 }
