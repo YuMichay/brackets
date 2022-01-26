7a7dcddccae8d2c045d6d6c31ef2f9641b6e167a
Yulia Mizhevich <julia.one.such@gmail.com>
2022-01-26 21:28:11 +0300

    fix: fixed solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
-module.exports = function check(str, bracketsConfig) {
-  const openBrackets = ['(', '{', '['];
-  const bracketCouples = {
-    [')']: '(',
-    ['}']: '{',
-    [']']: '['
-  };
-  let result = [];
-  for (let i = 0; i < str.length; i++){
-    let strSymbol = str[i];
-    if (openBrackets.includes(strSymbol)){
-      result.push(strSymbol);
-    } else{
-      if (result.length===0){
-        return false;
-      }
-      let lastElement = result[result.length-1];
-      if (bracketCouples[strSymbol]===lastElement){
-        result.pop();
-      } else{
-        return false;
+module.exports = function check(str, config) {
+  let stack = [];
+  let openingBrackets = [];
+  let closingBrackets = [];
+  for(let i = 0; i < config.length; i++) {
+    for(let j = 0; j < config[i].length; j++) {
+      if (j === 0) {
+        openingBrackets.push(config[i][j]);
+      } else if (j%2 === 0) {
+        openingBrackets.push(config[i][j]);
+      } else if (j%2 !== 0) {
+        closingBrackets.push(config[i][j]);
       }
     }
   }
-  return result.length===0; 
+  for (let i = 0; i < str.length; i++) {
+    let checkedSymb = str[i];
+    if (str.length%2 !== 0) {
+      return false;
+    }
+    if (openingBrackets.includes(checkedSymb)) {
+      stack.push(checkedSymb);
+    }
+    let lastSymb = stack[stack.length-1];
+    if (openingBrackets.indexOf(checkedSymb) === closingBrackets.indexOf(lastSymb)) {
+      stack.pop(checkedSymb);
+    }
+  }
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
